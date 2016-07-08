cb992378ab7090db1ee9b5b5a935dbbda3e86c9e
bikrus <bikrus@e.mail>
2016-07-08 20:22:51 +0300

    Slightly changed html. Separate behaviour for Firefox.

--- index.js
+++ index.js
@@ -7,8 +7,25 @@
   var stickyCommon = function(el, elAnchor, top) {
     var requiredOriginalStyles = ['position', 'top', 'left'];
 
+    const POSITION_RELATIVE = 0;
+    const POSITION_FIXED = 1;
+
     var requiredTop = top || 0;
     var originalRect = calcRect(el);
+    var lastPosition = POSITION_RELATIVE;
+    var getLastPosition = (function(){
+      return(function(){
+        return(lastPosition);
+      });
+    })();
+    var setLastPosition = (function(){
+      return(function(newLastPosition){
+        lastPosition = newLastPosition;
+      });
+    })();
+
+
+    var lastWindowTop = 0;
     var styles = {
       position: 'fixed',
       top: requiredTop + 'px',
@@ -21,43 +38,39 @@
       originalStyles[key] = el.style[key];
     });
 
-    var onscroll;
-    if (window.onscroll) {
-      onscroll = window.onscroll;
-    }
-
-    window.onscroll = function(event) {
+    function calcElementPosition(msg){
+      el.innerHTML += msg;
       var windowScroll = getWindowScroll();
-      if (windowScroll.top > originalRect.top - requiredTop) {
-        for (key in styles) {
-          var value = styles[key];
-          if (key === "left") {
-            var rect = calcRect(elAnchor);
-            value = rect.rawLeft + "px";
+        var lastPosition = getLastPosition();
+        if (lastPosition == POSITION_RELATIVE && windowScroll.top > originalRect.top - requiredTop) {
+          setLastPosition(POSITION_FIXED);
+          for (key in styles) {
+            var value = styles[key];
+            if (key === "left") {
+              var rect = calcRect(elAnchor);
+              value = rect.rawLeft + "px";
+            }
+            el.style[key] = value;
           }
-          el.style[key] = value;
+        } else if (lastPosition == POSITION_FIXED && windowScroll.top <= originalRect.top - requiredTop) {
+          setLastPosition(POSITION_RELATIVE);
+          for (key in originalStyles) {
+            el.style[key] = originalStyles[key];
+          }
         }
-      } else {
-        for (key in originalStyles) {
-          el.style[key] = originalStyles[key];
-        }
-      }
-      onscroll && onscroll(event)
     }
 
-    var onresize;
-    if (window.onresize) {
-      onresize = window.onresize;
-    }
+//     window.addEventListener("touchmove", function(){calcElementPosition("A");});
 
-    window.onresize = function(event) {
-      onresize && onresize(event)
+    window.addEventListener("scroll", function(){calcElementPosition("");});
+
+    window.addEventListener("resize", function(){
       if (getWindowScroll().top > originalRect.top - requiredTop) {
         var rect = calcRect(elAnchor);
         value = rect.rawLeft + "px";
         el.style["left"] = value;
       }
-    }
+    });
 
     function calcRect(el) {
       var rect = el.getBoundingClientRect();
@@ -106,11 +119,20 @@
     }
   }
 
+  var stickyFirefox = function(el, elAnchor, top) {
+    el.innerHTML = "Firefox";
+    el.style["z-index"] = 9999 + el.getBoundingClientRect().top;
+    el.style["position"] = "sticky";
+    el.style["top"] = (top || 0) + "px";
+  }
+
   var ua = navigator.userAgent;
   var stickyFunction = stickyCommon;
 
   if ((/Android/i.test(ua)) && (/Chrome/i.test(ua)) && !(/Version/i.test(ua))){
     stickyFunction = stickyAndroidChrome;
+  } else if ((/Firefox/i.test(ua))){
+    stickyFunction = stickyFirefox;
   }
 
   return(stickyFunction);
